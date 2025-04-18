b97b6fa7a79cc7ab56ed2ff76e782a0837806871
iagoscripttest3 <41646645-iagoscripttest3@users.noreply.replit.com>
2025-04-18 21:18:29 +0000

    corrigido connection.js com base64

--- firebase/connection.js
+++ firebase/connection.js
@@ -1,17 +1,23 @@
-
 const admin = require('firebase-admin');
 require('dotenv').config();
 
+const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_B64, 'base64').toString('utf8');
+
 const credentials = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: decodedKey,
 };
 
-admin.initializeApp({
-  credential: admin.credential.cert(credentials)
-});
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+  });
+
+  console.log('[Firebase] Conectado com sucesso.');
+} catch (error) {
+  console.error('[Firebase] ERRO AO CONECTAR:', error.message);
+}
 
 const db = admin.firestore();
-
 module.exports = db;
